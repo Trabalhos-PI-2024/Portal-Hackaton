aefc7e87a45f3dbe96d14e91e508ca29aec68dcb
EnzoGirardi <enzogirardi1903@gmail.com>
2024-07-23 14:37:55 -0300

    Feat: Informações Enzo #6

---
+++ src/views/EnzoView.vue
@@ -0,0 +1,21 @@
+<template>
+  <main>
+    <div class="enzo">
+      <div class="imagem">
+        <h1>imagem aqui</h1>
+      </div>
+      <div class="info">
+        <h1>Enzo Girardi</h1>
+        <h2>Informações:</h2>
+        <p>Instituto Federal Catarinense: Técnico em Informática</p>
+        <p>enzogirardi1903@gmail.com</p>
+        <p>16 anos</p>
+        <ion-icon name="logo-github"><a href="https://github.com/EnzoGirardi"></a></ion-icon>
+        <ion-icon name="logo-linkedin"><a href="https://www.linkedin.com/in/enzo-girardi-81b50929a/"></a></ion-icon>
+        <ion-icon name="logo-instagram"><a href="https://www.instagram.com/_.enzo7._/"></a></ion-icon>
+        <p></p>
+      </div>
+    </div>
+  </main>
+</template>
+
