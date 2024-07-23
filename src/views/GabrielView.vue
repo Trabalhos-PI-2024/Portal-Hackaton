848f42ce5dc1f012a2b39610aa9f0d61340a34f8
gabrielmartinsgomes <gabrielhenrickmarind3454@gmail.com>
2024-07-23 14:28:18 -0300

    FEAT: informações Gabriel

---
+++ src/views/GabrielView.vue
@@ -0,0 +1,22 @@
+<template> 
+<div class="gabriel">
+      <h1>This is an contact page</h1>
+      <div class="imagem">
+        <h1>imagem aqui</h1>
+      </div>
+      <div class="info">
+        <h1>Gabriel Martins</h1>
+        <h2>Informações:</h2>
+        <p>Instituto Federal Catarinense: Técnico em Informática</p>
+        <p>gabrielhenrickmartind3454@gmail.com</p>
+        <p>16 anos</p>
+        <ion-icon name="logo-github"><a href="https://github.com/gabrielmartinsgomes"></a></ion-icon>
+        <ion-icon name="logo-linkedin"><a href="https://www.linkedin.com/in/gabriel-henrick-martins-gomes-27230b299/"></a></ion-icon>
+        <ion-icon name="logo-instagram"><a href="https://www.instagram.com/notgbi/"></a></ion-icon>
+        <p></p>
+      </div>
+    </div>
+</template>
+<style scoped>
+
+</style>
