7b073a159d70287c0023e782ec7aa6913baafa5b
DaviBisewski <davimbisewski@gmail.com>
2024-07-23 14:56:30 -0300

    Feat: Ajustes #4

--- src/views/DaviView.vue
+++ src/views/DaviView.vue
@@ -2,7 +2,7 @@
   <main>
     <div class="davi">
       <div class="imagem">
-        <h1>imagem aqui</h1>
+        <img src="" alt="Imagem Aqui">
       </div>
       <div class="info">
         <h1>Davi Bisewski</h1>
@@ -10,10 +10,10 @@
         <p>Instituto Federal Catarinense: Técnico em Informática</p>
         <p>davimbisewski@gmail.com</p>
         <p>16 anos</p>
+        <p>Joinville - Santa Catarina</p>
         <ion-icon name="logo-github"><a href="https://github.com/DaviBisewski"></a></ion-icon>
         <ion-icon name="logo-linkedin"><a href="https://www.linkedin.com/in/davi-de-morais-bisewski-110906299/"></a></ion-icon>
         <ion-icon name="logo-instagram"><a href="https://www.instagram.com/davi_bisewski/"></a></ion-icon>
-        <p></p>
       </div>
     </div>
   </main>
