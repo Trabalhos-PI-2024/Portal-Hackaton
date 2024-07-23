db90271c185613fd475417475e03fbb4c2121682
gabrielmartinsgomes <gabrielhenrickmarind3454@gmail.com>
2024-07-23 14:38:46 -0300

    FEAT: cidade e estado

--- src/views/GabrielView.vue
+++ src/views/GabrielView.vue
@@ -10,10 +10,10 @@
         <p>Instituto Federal Catarinense: Técnico em Informática</p>
         <p>gabrielhenrickmartind3454@gmail.com</p>
         <p>16 anos</p>
+        <p>Joinville - Santa Catarina</p>
         <ion-icon name="logo-github"><a href="https://github.com/gabrielmartinsgomes"></a></ion-icon>
         <ion-icon name="logo-linkedin"><a href="https://www.linkedin.com/in/gabriel-henrick-martins-gomes-27230b299/"></a></ion-icon>
         <ion-icon name="logo-instagram"><a href="https://www.instagram.com/notgbi/"></a></ion-icon>
-        <p></p>
       </div>
     </div>
 </template>
