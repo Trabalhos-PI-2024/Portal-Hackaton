b2c20e58e76a2ef77f0b820ba36800c7fcb9b086
lotinn <larissaslotin@gmail.com>
2024-07-26 14:02:06 -0300

    FEAT: Página Larissa #7

---
+++ src/views/LarissaView.vue
@@ -0,0 +1,22 @@
+<template> 
+  <div class="larissa">
+        <h1>This is an contact page</h1>
+        <div class="imagem">
+          <h1>imagem aqui</h1>
+        </div>
+        <div class="info">
+          <h1>Larissa Lotin</h1>
+          <h2>Informações:</h2>
+          <p>Instituto Federal Catarinense: Técnico em Informática</p>
+          <p>larissaslotin@gmail.com</p>
+          <p>16 anos</p>
+          <p>Joinville - Santa Catarina</p>
+          <ion-icon name="logo-github"><a href="https://github.com/lotinn"></a></ion-icon>
+          <ion-icon name="logo-linkedin"><a href="https://www.linkedin.com/in/larissa-lotin-46997429a/?originalSubdomain=br"></a></ion-icon>
+          <ion-icon name="logo-instagram"><a href="https://www.instagram.com/lariilotin/"></a></ion-icon>
+        </div>
+      </div>
+  </template>
+  <style scoped>
+  
+  </style>
