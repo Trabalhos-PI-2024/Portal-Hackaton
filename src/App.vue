1c125386c6ea18355a87dab81eb8bdb8f6227b9f
HenriqueChepli <henriquechepli@gmail.com>
2024-07-29 11:02:43 -0300

    FEAT: Adicionado img #4

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,6 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
- 
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/davi">Davi</RouterLink>
-      </nav>
- 
-
   <RouterView />
 </template>
 
--- src/views/DaviView.vue
+++ src/views/DaviView.vue
@@ -1,20 +1,38 @@
 <template>
-  <main>
-    <div class="davi">
-      <div class="imagem">
-        <img src="" alt="Imagem Aqui">
+  <main class="main">
+    <header class="header">
+        <div class="title">
+            <router-link to="/"><h1>Portal Hackaton</h1></router-link>
+        </div>
+     <div class="nav">
+      <RouterLink to="/davi"><p>Davi</p></RouterLink>
+      <RouterLink to="/enzo"><p>Enzo</p></RouterLink>
+      <RouterLink to="/gabriel"><p>Gabriel</p></RouterLink>
+      <RouterLink to="/henrique"><p>Henrique</p></RouterLink>
+      <RouterLink to="/larrisa"><p>Larissa</p></RouterLink>
+      <RouterLink to="/murilo"><p>Murilo</p></RouterLink>
+      </div>
+    </header>
+    <div class="mainPrincipal">
+      <div class="redesSociais">
+        <a href="https://github.com/DaviBisewski"> <ion-icon name="logo-github"></ion-icon></a>
+        <a href="https://www.linkedin.com/in/davi-de-moraes-bisewski-110906299/"
+          ><ion-icon name="logo-linkedin"></ion-icon
+        ></a>
+        <a href="https://www.instagram.com/davi_bisewski/"><ion-icon name="logo-instagram"></ion-icon></a>
       </div>
       <div class="info">
-        <h1>Davi Bisewski</h1>
-        <h2>Informações:</h2>
-        <p>Instituto Federal Catarinense: Técnico em Informática</p>
         <p>davimbisewski@gmail.com</p>
+        <h1>Dai Bisewski</h1>
         <p>16 anos</p>
+        <p>Instituto Federal Catarinense: Técnico em Informática</p>
         <p>Joinville - Santa Catarina</p>
-        <ion-icon name="logo-github"><a href="https://github.com/DaviBisewski"></a></ion-icon>
-        <ion-icon name="logo-linkedin"><a href="https://www.linkedin.com/in/davi-de-morais-bisewski-110906299/"></a></ion-icon>
-        <ion-icon name="logo-instagram"><a href="https://www.instagram.com/davi_bisewski/"></a></ion-icon>
       </div>
+      <article class="article">
+        <div class="imagem">
+          <img src="../assets/img/davi.jpeg" alt="Imagem Aqui" />
+        </div>
+      </article>
     </div>
   </main>
 </template>
