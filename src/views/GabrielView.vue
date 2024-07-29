1404b25f00e489254b364407d55669952f1b3fd7
gabrielmartinsgomes <gabrielhenrickmartind3454@gmail.com>
2024-07-29 15:56:42 -0300

    FEAT: imagem do membro Gabriel

--- src/views/GabrielView.vue
+++ src/views/GabrielView.vue
@@ -1,9 +1,6 @@
 <template> 
 <div class="gabriel">
       <h1>This is an contact page</h1>
-      <div class="imagem">
-        <h1>imagem aqui</h1>
-      </div>
       <div class="info">
         <h1>Gabriel Martins</h1>
         <h2>Informações:</h2>
@@ -14,6 +11,11 @@
         <ion-icon name="logo-github"><a href="https://github.com/gabrielmartinsgomes"></a></ion-icon>
         <ion-icon name="logo-linkedin"><a href="https://www.linkedin.com/in/gabriel-henrick-martins-gomes-27230b299/"></a></ion-icon>
         <ion-icon name="logo-instagram"><a href="https://www.instagram.com/notgbi/"></a></ion-icon>
+        <article class="article">
+        <div class="imagem">
+          <img src="../assets/img/gb.jpeg" alt="Imagem Aqui" />
+        </div>
+      </article>
       </div>
     </div>
 </template>
