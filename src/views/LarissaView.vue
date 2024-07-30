8a5908ebdd9ab3a9f6345f2f9284b7323df973a3
larissa lotin <131203128+lotinn@users.noreply.github.com>
2024-07-29 22:10:53 -0300

    FEAT: foto página larissa #7

--- src/views/LarissaView.vue
+++ src/views/LarissaView.vue
@@ -1,9 +1,6 @@
 <template> 
   <div class="larissa">
         <h1>This is an contact page</h1>
-        <div class="imagem">
-          <h1>imagem aqui</h1>
-        </div>
         <div class="info">
           <h1>Larissa Lotin</h1>
           <h2>Informações:</h2>
@@ -14,6 +11,11 @@
           <ion-icon name="logo-github"><a href="https://github.com/lotinn"></a></ion-icon>
           <ion-icon name="logo-linkedin"><a href="https://www.linkedin.com/in/larissa-lotin-46997429a/?originalSubdomain=br"></a></ion-icon>
           <ion-icon name="logo-instagram"><a href="https://www.instagram.com/lariilotin/"></a></ion-icon>
+          <article class="article"> 
+          <div class="imagem"> 
+          <img src="../assets/img/larissa.jpg" alt="Imagem Aqui" /> 
+          </div> 
+          </article>
         </div>
       </div>
   </template>
